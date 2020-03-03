0fdb33af0056851650e02f179c984c62fd55bd6c
alexRCallies <59710714+alexRCallies@users.noreply.github.com>
2020-03-03 15:57:24 -0600

    got a trable to print

--- Client/app.js
+++ Client/app.js
@@ -23,18 +23,25 @@
 
         e.preventDefault();
     }
-
     $('#my-form').submit( processForm );
-
 })(jQuery); 
-$(document).ready(function () {
-    $.ajax({
-        url: "https://localhost:44325/api/movie"
-    }).done(function (data) {
-        console.log(data);
-        $('#movieTitles')
-            .append(data.map(function (val) {
-                return "<td>" + val.Title + "</td>"
-            }));
+$(document).ready(function() {
+    var movie_data = "";
+    $.getJSON("https://localhost:44325/api/movie", function(data) {
+      $.each(data, function(key, value) {
+        movie_data +=
+          "<tr>" +
+          "<td>" +
+          value.title +
+          "</td>" +
+          "<td>" +
+          value.genre +
+          "</td>" +
+          "<td>" +
+          value.director +
+          "</td>" +
+          "</tr>";
+      });
+      $("#Results_Table").append(movie_data);
     });
-});
+  });
